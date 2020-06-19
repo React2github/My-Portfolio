3395160f6551194fcc474c5bcc20a37e76f0baab
Abdella <abdella.shukur2@gmail.com>
2020-06-19 14:59:52 -0400

    fixed centering of text of bio component

--- src/Componets/bio.js
+++ src/Componets/bio.js
@@ -10,7 +10,7 @@ class Bio extends Component {
  <div className="jumbotron" id="Home">
     <div id="info">
     <img src={"/img/profile.jpg"} alt="profile pic"></img>
-    <h1 className="display-4 center">Abdella Shukur</h1>
+    <h1 className="center">Abdella Shukur</h1>
     <p className="lead center">A builder and student of technology</p>
     <p className="center">Atlanta, GA | Full-stack developer</p>
     <a className="btn btn-primary btn-lg" href="#Contact" role="button">Contact</a>
