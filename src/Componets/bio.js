5a143e66454156c08a5ac3198dbf418b48dd75ad
Abdella <abdella.shukur2@gmail.com>
2020-06-19 19:05:10 -0400

    added modal and contact info

--- src/Componets/bio.js
+++ src/Componets/bio.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/bio.css";
 
+function Linkedin() {
+    window.open("https://www.linkedin.com/in/abdella-abdushukur-2b7b42144/");
+  }
+  
+  function Github() {
+    window.open("https://github.com/React2github");
+  }
 
 class Bio extends Component {
     state = {  }
@@ -13,11 +20,31 @@ class Bio extends Component {
     <h1 className="center">Abdella Shukur</h1>
     <p className="lead center">A builder and student of technology</p>
     <p className="center">Atlanta, GA | Full-stack developer</p>
-    <a className="btn btn-primary btn-lg" href="#Contact" role="button">Contact</a>
+    <button className="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal">Contact</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Let's Grab Coffee</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          Email: abdella.shukur2@gmail.com <br />
+          Call: 404-242-3591 <br />
+          Reach me on <button onClick={Linkedin.bind(this)}>Linkedin</button> or <button onClick={Github.bind(this)}>Github</button> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
     </div>
+  </div>
+</div>
+ </div>
 </div> 
  );
     }
 }
- 
+
 export default Bio;
